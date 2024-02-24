3db5b352171031e27a440a7d353457131a40c5d7
alexpanmk <alexpanmk@gmail.com>
2024-02-24 13:21:37 +0800

    set up course model

---
+++ daos/courses.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+// optional shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema;
+
+// Define a schema for Quiz Questions
+const QuestionSchema = new Schema({
+  questionText: {
+    type: String,
+    required: true
+  },
+  options: [{
+    type: String,
+    required: true
+  }],
+  correctAnswer: {
+    type: String,
+    required: true
+  }
+});
+
+// Define the main Quiz Schema
+const CourseSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  questions: [QuestionSchema] // Embedding questions directly in the quiz document
+});
+
+// Compile the schema into a model
+const Quiz = mongoose.model('Course', CourseSchema);
+
+module.exports = Course;
+  
+
+
+
